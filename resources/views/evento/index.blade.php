7f2dd3e1bedb8f41db061f6e9956ff343abf721b
jramirez18 <jorramirez_18@hotmail.com>
2021-05-23 17:39:57 -0600

    Se creo a ruta de EVENTO

--- resources/views/evento/index.blade.php
+++ resources/views/evento/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
 <div id="agenda">
-
+    <h1>CALENDARIO</h1>
 </div>
 </div>
 @endsection 
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/evento', [App\Http\Controllers\EventoController::class, 'index']);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
