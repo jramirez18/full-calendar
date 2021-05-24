21be439de73a020f83f7b3b0796d02422fb3e95c
jramirez18 <jorramirez_18@hotmail.com>
2021-05-23 19:23:48 -0600

    Se cambio el idioma al calendario

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('agenda');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
+      initialView: 'dayGridMonth',
+      
+      //cambiamos idioma al calendario
+      locale:"es"
     });
     calendar.render();
   });
