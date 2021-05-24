c5bbb2cab5a3d7c88bca0aed8df2c6cb9177e6a1
jramirez18 <jorramirez_18@hotmail.com>
2021-05-23 20:42:21 -0600

    Se modifico las opciones del calendario como ver por Mes, semana, agenda

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -2,9 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('agenda');
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      
+
       //cambiamos idioma al calendario
-      locale:"es"
+      locale:"es",
+
+      //diferentes vistas
+      headerToolbar:{
+          left: 'prev,next,today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,listWeek'
+      },
     });
     calendar.render();
   });
